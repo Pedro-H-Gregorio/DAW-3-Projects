84b75600d18287142a0b6dcd580937d5b8c9da7d
Pedro Henrique Gregório <pedrosgregorio@gmail.com>
2025-04-23 22:01:21 -0300

    Organizando pastas

---
+++ daily/diario/components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+import Form from "../misc/Form";
+
+export default function Footer() {
+  return (
+    <footer id="footer">
+      <section>
+        <Form />
+      </section>
+    </footer>
+  );
+}
