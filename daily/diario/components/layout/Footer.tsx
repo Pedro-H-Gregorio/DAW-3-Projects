c16f5f0607fcc3c503d7501a73611d7cadee82f6
Pedro Henrique Gregório <pedrosgregorio@gmail.com>
2025-04-23 22:49:29 -0300

    Removendo create on post

--- daily/diario/components/layout/Footer.tsx
+++ daily/diario/components/layout/Footer.tsx
@@ -1,11 +1,13 @@
+"use client";
+import { usePathname } from "next/navigation";
 import Form from "../misc/Form";
 
 export default function Footer() {
+  const pathName = usePathname();
+  console.log(usePathname());
   return (
     <footer id="footer">
-      <section>
-        <Form />
-      </section>
+      <section>{!(pathName == "post") || <Form />}</section>
     </footer>
   );
 }
